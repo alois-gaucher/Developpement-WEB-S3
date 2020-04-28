986deef226be85d50729256455aa7d95ebe57a4c
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-04-28 14:02:11 +0200

    Création d'un "MOTD" dynamique - FONCTIONNEL - Change toutes les 5s

--- application/views/Accueil_view.php
+++ application/views/Accueil_view.php
@@ -50,9 +50,6 @@
 	  </div>
 	</div>
 	<!-- Jumbotron -->
-	<div class="progress md-progress">
-    <div class="progress-bar progress-bar-striped progress-bar-animated bg-dark" role="progressbar" style="width: 5%" aria-valuenow="5" aria-valuemin="0" aria-valuemax="100"></div>
-	</div>
 	<!-- Footer -->
 	<footer class="page-footer fixed-bottom font-small special-color-dark pt-4">
 
@@ -98,17 +95,24 @@
 	</script>
 	<script type="text/javascript">
 		$(document).ready(function() {
-			var r_text = new Array ();
-			r_text[0] = "Ah non attention, ce n'est pas un simple sport car on vit dans une réalité qu'on a créée et que j'appelle illusion et finalement tout refaire depuis le début. Ça respire le meuble de Provence, hein ?";
-			r_text[1] = "Tu vois, là on voit qu'on a beaucoup à travailler sur nous-mêmes car entre penser et dire, il y a un monde de différence et c'est très, très beau d'avoir son propre moi-même ! Mais ça, c'est uniquement lié au spirit.";
-			r_text[2] = "Ça sounds good, je suis mon meilleur modèle car on vit dans une réalité qu'on a créée et que j'appelle illusion et ça, c'est très dur, et, et, et... c'est très facile en même temps. C'est cette année que j'ai eu la révélation !";
-			r_text[3] = "You see, après il faut s'intégrer tout ça dans les environnements et on est tous capables de donner des informations à chacun puisque the final conclusion of the spirit is perfection Et j'ai toujours grandi parmi les chiens.";
-			r_text[4] = "You see, je suis mon meilleur modèle car entre penser et dire, il y a un monde de différence parce que spirituellement, on est tous ensemble, ok ? Mais ça, c'est uniquement lié au spirit.";
-			r_text[5] = "Tu vois, ce n'est pas un simple sport car il faut toute la splendeur du aware et finalement tout refaire depuis le début. C'est cette année que j'ai eu la révélation !";
+			function motd() {
+				var r_text = new Array ();
+				r_text[0] = "Ah non attention, ce n'est pas un simple sport car on vit dans une réalité qu'on a créée et que j'appelle illusion et finalement tout refaire depuis le début. Ça respire le meuble de Provence, hein ?";
+				r_text[1] = "Tu vois, là on voit qu'on a beaucoup à travailler sur nous-mêmes car entre penser et dire, il y a un monde de différence et c'est très, très beau d'avoir son propre moi-même ! Mais ça, c'est uniquement lié au spirit.";
+				r_text[2] = "Ça sounds good, je suis mon meilleur modèle car on vit dans une réalité qu'on a créée et que j'appelle illusion et ça, c'est très dur, et, et, et... c'est très facile en même temps. C'est cette année que j'ai eu la révélation !";
+				r_text[3] = "You see, après il faut s'intégrer tout ça dans les environnements et on est tous capables de donner des informations à chacun puisque the final conclusion of the spirit is perfection Et j'ai toujours grandi parmi les chiens.";
+				r_text[4] = "You see, je suis mon meilleur modèle car entre penser et dire, il y a un monde de différence parce que spirituellement, on est tous ensemble, ok ? Mais ça, c'est uniquement lié au spirit.";
+				r_text[5] = "Tu vois, ce n'est pas un simple sport car il faut toute la splendeur du aware et finalement tout refaire depuis le début. C'est cette année que j'ai eu la révélation !";
 
-			var i = Math.floor(r_text.length * Math.random());
+				var i = Math.floor(r_text.length * Math.random());
 
-			document.getElementById('motd').innerHTML = r_text[i];
+				document.getElementById('motd').innerHTML = r_text[i];
+
+				console.log("Changement de motd...");
+				x = 5;
+				setTimeout(motd, x*1000);
+			}
+			motd();
 		});
 	</script>
 </body>
