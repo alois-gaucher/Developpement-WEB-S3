87f170c21ee2430e4a8e71d1f790357d8a9f31a6
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-04-30 08:19:54 +0200

    Ajout du controlleur salles

---
+++ application/controllers/Salles.php
@@ -0,0 +1,11 @@
+<?php
+class Salles exetends CI_Controller {
+	public function index()
+	{
+		$this->load->model('Salles_model');
+		$data['toutes'] = $this->Salles_model->toutesLesSalles();
+			$this->load->view('Salles_view', $data);
+
+		}
+}
+?>
