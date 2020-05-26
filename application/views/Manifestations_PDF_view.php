cf5902cdaac74c9cbd56e0875e80504c7064a1ed
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-05-26 09:32:52 +0200

    * Affichage des manifestations dans le catalogue PDF

--- application/views/Manifestations_PDF_view.php
+++ application/views/Manifestations_PDF_view.php
@@ -13,5 +13,26 @@ $pdf->Image('assets/photos/logo_stpavut.png', 35, 80, 0);
 $pdf->SetXY(0, 265);
 $pdf->Cell(0, 10, 'Imprimé le '.date("j F Y, H:m:s"), 0, 1, "C");
 
+$pdf->SetAutoPageBreak(1, 15);
+$pdf->AddPage();
+
+foreach ($toutes as $une)
+{
+	$pdf->SetFillColor(15, 0, 0);
+	$pdf->SetTextColor(255, 255, 255);
+	$pdf->Cell(0, 15, $une->manif_intitule, 0, 1, "C", 1);
+	$pdf->WriteHTML($une->manif_prix_place.' €<br>');
+	$y = $pdf->GetY();
+	$pdf->Image('assets/photos/'.$une->manif_photo, 50, $y, 30);
+	if($y>150)
+	{
+		$pdf->AddPage();
+	}
+	else
+	{
+		$pdf->SetY($y + 50);
+	}
+}
+
 $pdf->Output('stpavut_catalogue.pdf');
 ?>
