ae6d7f049064956adf4b1ea01d55751c7a41a519
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-05-27 15:29:01 +0200

    * Modification du comportement au niveau des PDF

--- application/views/Manifestations_PDF_view.php
+++ application/views/Manifestations_PDF_view.php
@@ -1,19 +1,11 @@
 <?php
 include('assets/tcpdf/tcpdf.php');
 $pdf = new tcpdf();
-$pdf->SetAutoPageBreak(0, 0);
 $pdf->addPage();
 
-$pdf->Cell(0, 10, 'CATALOGUE Saint Pavut', 1, 1, "C");
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+$pdf->Image(''.base_url().'assets/photos/couverture.jpg', 0, 0);
 
-$pdf->writeHTML('<p><strong>Voici notre catalogue</strong></p>');
-
-$pdf->Image('assets/photos/logo_stpavut.png', 35, 80, 0);
-
-$pdf->SetXY(0, 265);
-$pdf->Cell(0, 10, 'Imprimé le '.date("j F Y, H:m:s"), 0, 1, "C");
-
-$pdf->SetAutoPageBreak(1, 15);
 $pdf->AddPage();
 
 foreach ($toutes as $une)
@@ -22,17 +14,12 @@ foreach ($toutes as $une)
 	$pdf->SetTextColor(255, 255, 255);
 	$pdf->Cell(0, 15, $une->manif_intitule, 0, 1, "C", 1);
 	$pdf->SetTextColor(0, 0, 0);
-	$pdf->WriteHTML($une->manif_prix_place.' €<br>');
-	$y = $pdf->GetY();
-	$pdf->Image('assets/photos/'.$une->manif_photo, 50, $y, 30);
-	if($y>150)
-	{
-		$pdf->AddPage();
-	}
-	else
-	{
-		$pdf->SetY($y + 50);
-	}
+	$pdf->SetXY(10, 115);
+	$pdf->WriteHTML('<b>Prix:</b> '.$une->manif_prix_place.' €');
+	$pdf->Image(''.base_url().'assets/photos/'.$une->manif_photo, 80, 30, 50);
+	$pdf->SetXY(10, 125);
+	$pdf->WriteHTML('<b>Description: </b>'.$une->manif_description.'');
+	$pdf->AddPage();
 }
 
 $pdf->Output('stpavut_catalogue.pdf');
